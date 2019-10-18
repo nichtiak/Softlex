691e3ea258bb69dfe55ae244f6c1b6725636994e
artem <40721180+nichtiak@users.noreply.github.com>
2019-10-18 18:03:17 +0500

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -42,70 +42,76 @@ document.addEventListener('DOMContentLoaded', () => {
 
   // Раздел "Подход" анимация окружности
 
-  let circles = document.querySelectorAll('#circles'),
-    ctx1 = circles[0].getContext('2d'),
-    ctx2 = circles[1].getContext('2d'),
-    ctx3 = circles[2].getContext('2d'),
-    ctx4 = circles[3].getContext('2d'),
-    endpoint = 270,
-    pi = Math.PI,
-    i;
+  // let circles = document.querySelectorAll('#circles'),
+  //   ctx1 = circles[0].getContext('2d'),
+  //   ctx2 = circles[1].getContext('2d'),
+  //   ctx3 = circles[2].getContext('2d'),
+  //   ctx4 = circles[3].getContext('2d'),
+  //   endpoint = 270,
+  //   pi = Math.PI,
+  //   i;
 
-  for (i = 0; i < circles.length; i++) {
-    //синие кружки
-    circles[i].getContext('2d').beginPath();
-    circles[i].getContext('2d').arc(30, 30, 5, 0, pi * 2);
-    circles[i].getContext('2d').fillStyle = '#005EF3';
-    circles[i].getContext('2d').fill();
+  // for (i = 0; i < circles.length; i++) {
+  //   //синие кружки
+  //   circles[i].getContext('2d').beginPath();
+  //   circles[i].getContext('2d').arc(30, 30, 5, 0, pi * 2);
+  //   circles[i].getContext('2d').fillStyle = '#005EF3';
+  //   circles[i].getContext('2d').fill();
 
-    //серые rруги
-    circles[i].getContext('2d').beginPath();
-    circles[i].getContext('2d').lineWidth = 5;
-    circles[i].getContext('2d').strokeStyle = '#F3F3F3';
-    circles[i].getContext('2d').arc(30, 30, 27.5, 0, pi * 2);
-    circles[i].getContext('2d').stroke();
-  }
+  //   //серые rруги
+  //   circles[i].getContext('2d').beginPath();
+  //   circles[i].getContext('2d').lineWidth = 5;
+  //   circles[i].getContext('2d').strokeStyle = '#F3F3F3';
+  //   circles[i].getContext('2d').arc(30, 30, 27.5, 0, pi * 2);
+  //   circles[i].getContext('2d').stroke();
+  // }
 
-  //Анимация красных окружностей
-  function draw361() {
-    
-      ctx1.beginPath();
-      ctx1.arc(30, 30, 27.5, pi / 180 * 270, pi / 180 * endpoint);
-      ctx1.lineWidth = 4;
-      ctx1.strokeStyle = '#F64D40';
-      ctx1.stroke();
+  // //Анимация красных окружностей
+  // function draw361() {
 
-      endpoint += 1;
-      if (endpoint == 361) {
-        endpoint = 270;
-      }
+  //     ctx1.beginPath();
+  //     ctx1.arc(30, 30, 27.5, pi / 180 * 270, pi / 180 * endpoint);
+  //     ctx1.lineWidth = 4;
+  //     ctx1.strokeStyle = '#F64D40';
+  //     ctx1.stroke();
 
-    requestAnimationFrame(draw361);
-  }
-  draw361();
-  
-  function draw451() {
+  //     endpoint += 1;
+  //     if (endpoint == 361) {
+  //       endpoint = 270;
+  //     }
 
-      ctx2.beginPath();
-      ctx2.arc(30, 30, 27.5, pi / 180 * 270, pi / 180 * endpoint);
+  //   requestAnimationFrame(draw361);
+  // }
+  // draw361();
 
-      ctx2.lineWidth = 4;
-      ctx2.strokeStyle = '#F64D40';
-      ctx2.stroke();
+  // function draw451() {
 
-      endpoint += 1;
-      if (endpoint == 451) {
-        endpoint = 270;
-      }
+  //     ctx2.beginPath();
+  //     ctx2.arc(30, 30, 27.5, pi / 180 * 270, pi / 180 * endpoint);
 
-    
-    requestAnimationFrame(draw451);
-  }
-  
-  draw451();
+  //     ctx2.lineWidth = 4;
+  //     ctx2.strokeStyle = '#F64D40';
+  //     ctx2.stroke();
+
+  //     endpoint += 1;
+  //     if (endpoint == 451) {
+  //       endpoint = 270;
+  //     }
 
 
+  //   requestAnimationFrame(draw451);
+  // }
 
+  // draw451();
+
+
+  anime({
+    targets: 'path',
+    // strokeDasharray: 10% 20%
+    strokeDashoffset: [anime.setDashoffset, 0],
+    easing: 'easeInOutSine',
+    duration: 1500
+  });
 
 
 
